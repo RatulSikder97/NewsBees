d6aab7acf54ea26af1f5ae7a78cd5fb2ed3e9d3b
iamratul <ratulsikder104@gmail.com>
2020-12-28 20:59:11 +0600

    index scaliton created

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +10,61 @@
     <link rel="stylesheet" href="asset/css/style.css">
     <title>NewsBee</title>
 </head>
+
 <body>
-    
+    <!-- header start -->
+    <header>
+        <h1 class="logo">News BEEs</h1>
+        <div class="user-setting">
+        
+        </div>
+    </header>
+    <!-- header end -->
+
+    <!-- main content section start -->
+    <section class="main-content">
+    <!-- bee news section start -->
+        <div class="bee-news">
+            <h2 class="section-title">
+                
+            </h2>
+            <div class="section-content">
+            
+            </div>
+        </div>
+    <!-- bee news section end -->
+
+     <!-- main news section start -->
+     <div class="main-news">
+            <h2 class="section-title">
+                
+            </h2>
+            <div class="section-content">
+            
+            </div>
+        </div>
+    <!-- main news section end -->
+
+     <!-- my collection section start -->
+     <div class="my-collection">
+            <h2 class="section-title">
+                
+            </h2>
+            <div class="section-content">
+            
+            </div>
+        </div>
+    <!--  my collection section end -->
+
+    </section>
+    <!-- main content section end -->
+
+    <!-- footer start-->
+    <footer>
+
+    </footer>
+    <!-- footer end-->
+
 </body>
+
 </html>
